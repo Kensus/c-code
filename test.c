1eccdb3bbaa27deff71154c04c6375d6d41b1221
Kensus <elkeino@gmail.com>
2018-05-15 22:35:26 +0300

    addes an algorithm to check if a given value is prime

--- test.c
+++ test.c
@@ -1,8 +1,28 @@
 #include<stdio.h>
 
-int main(){
+int
+main ()
+{
 
-printf("Kenino annti matti");
-return 0;
+  int num, i;
+
+  printf ("Give a number to  check if a number is prime ");
+  scanf ("%d", &num);
+
+  for (i = 2; i < num; ++i)
+    {
+      if (num % 2 == 0)
+	{
+	  printf ("is not prime");
+	}
+      else
+	{
+	  printf ("is  prime");
+	}
+
+    }
+
+
+  return 0;
 
 }
